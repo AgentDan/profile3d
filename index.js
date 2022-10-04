d488f653fbff16b511cb8e345bf3ac39f2be741a
AgentDan <pregrada@mail.com>
2022-10-04 23:52:09 +0500

    com2

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require("express")
-const mongoose = require("mongoose")
 require('dotenv').config()
 const cors = require("cors")
+const path = require("path");
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json({extended: true}))
-// app.use('/3d', require('./routes/3d'))
 
-async function start() {
-    try {
-        await mongoose.connect("mongodb+srv://dan:admin@myclaster.uhkan.mongodb.net/dmodel?retryWrites=true&w=majority", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        app.listen(PORT, () => {console.log(`Server started in port ${PORT} `)}
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './client/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
         )
-    }catch (e) {console.log(`My ERROR ${e}`)}
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
 }
-start()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
